14b25467d7ae0a8dddce582213173fccf0da2ab4
VC <sr3867140@gmail.com>
2023-03-02 16:57:38 +0530

    express route

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const router = require('./routes/index')
+
+app.use('/', router )
 
 app.listen(port, () => {
   console.log(`facebook app listening on port ${port}`)
